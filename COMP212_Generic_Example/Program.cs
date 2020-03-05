dfb14bd20c5620130303f9e5061c7961dda95431
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-03-04 21:14:06 -0500

    Add project files.

---
+++ COMP212_Generic_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP212_Generic_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
