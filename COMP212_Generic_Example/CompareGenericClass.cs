fac828aa180e4e5882c45c9b87ba0ce718209538
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-03-04 21:21:08 -0500

    Created a generic class with a method

---
+++ COMP212_Generic_Example/CompareGenericClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP212_Generic_Example
+{
+    public class CompareGenericClass<T>
+    {
+        public bool Compare(T a, T b)
+        {
+            if (a.Equals(b))
+                return true;
+            else
+                return false;
+        }
+     }
+}
