102d4dd8b63222440a34c60778d14e2d4100e5b7
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-03-04 21:27:25 -0500

    created objects of two classes and called methods in main

--- COMP212_Generic_Example/Program.cs
+++ COMP212_Generic_Example/Program.cs
@@ -6,7 +6,22 @@ namespace COMP212_Generic_Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //creating object of non generic class
+            CompareClass compareClass = new CompareClass();
+            bool intResult = compareClass.Compare(5, 7);
+            Console.WriteLine(intResult);
+            bool stringResult = compareClass.Compare("One", "One");
+            Console.WriteLine(stringResult);
+
+            //creating objec of generic class
+            CompareGenericClass<int> compareGeneric = new CompareGenericClass<int>();
+            bool compareIntegers = compareGeneric.Compare(12, 18);
+            Console.WriteLine(compareIntegers);
+            CompareGenericClass<string> stringGeneric = new CompareGenericClass<string>();
+            bool compareStrings = stringGeneric.Compare("Two", "Two");
+            Console.WriteLine(compareStrings);
+
+            Console.ReadLine();
         }
     }
 }
