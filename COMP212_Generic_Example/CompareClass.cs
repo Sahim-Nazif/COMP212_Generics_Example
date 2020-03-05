0a88293c543238d4c8ea8b5411bf5050d903d883
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-03-04 21:18:54 -0500

    created a non-generic class and methods

---
+++ COMP212_Generic_Example/CompareClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP212_Generic_Example
+{
+    //using non-generic class and methods
+    public class CompareClass
+    {
+        public bool Compare(string a, string b)
+        {
+            if (a.Equals(b))
+                return true;
+            else
+                return false;
+        }
+        public bool Compare(int a, int b)
+        {
+            if(a.Equals(b))
+                return true;
+            else
+                return false;
+        }
+    }
+}
